a653eb59f7715fd3c5a5327485a2692b234e3378
Pheranmie <tobi.okekunle@reventtechnologies.com>
2022-10-01 16:30:22 +0100

    configured pg to connect to db

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 require("dotenv").config();
-import { BigNumber, Contract, providers, Wallet } from "ethers";
+import { BigNumber, Contract, providers } from "ethers";
+
+import pool from './dbconfig';
 
 
 import seaportABI from './abi/seaport.json'; // ABI for the seaport contract
@@ -10,9 +12,20 @@ const seaportContract = "0x00000000006c3852cbEf3e08E8dF289169EdE581";
 
 const ETHEREUM_RPC_URL = "wss://eth-mainnet.g.alchemy.com/v2/" + apikey;
 
-
-
 const provider = new providers.WebSocketProvider(ETHEREUM_RPC_URL);
+
+const connectToDb = async () => {
+  try {
+    const client = await pool.connect();
+    console.log("Connected to DB");
+    client.release();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+connectToDb();
 
 const getData = async () => {
   const contract = new Contract(seaportContract, seaportABI, provider);
@@ -26,7 +39,7 @@ const getData = async () => {
       offer: offer,
       data: event,
     };
-    
+
     console.log(JSON.stringify(eventResults, null, 7));
   });
 }
