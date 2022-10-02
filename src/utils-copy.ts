b722a34e3b2e2a05bd6a307210e52401c7db7c59
Pheranmie <tobi.okekunle@reventtechnologies.com>
2022-10-02 04:10:30 +0100

    modified utils-copy

--- src/utils-copy.ts
+++ src/utils-copy.ts
@@ -22,38 +22,29 @@ export async function createSaleRecordOnDB(saleObject: any) {
     console.error(err);
   }
 }
-// export async function createSaleOnDB(sale: any) {
-//   const client = await pool.connect();
-//   const query = {
-//     text: 'INSERT INTO sales(order_hash, offerer, zone, recipient, offer, consideration, data) VALUES($1, $2, $3, $4, $5, $6, $7)',
-//     values: [sale.orderHash, sale.offerer, sale.zone, sale.recipient, sale.offer, sale.consideration, sale.data],
-//   };
-//   client.query(query, (err, res) => {
-//     console.log(err, res);
-//     client.release();
-//   });
-// }
-
 export function hexToDecimal(hex: string) {
   return parseInt(hex, 16);
 }
 
 // convert wei to ether
 export function weiToEther(wei: string) {
-  if (wei !== undefined) {
-    const weiString = wei.toString();
-    const weiBN = ethers.BigNumber.from(weiString);
-    const etherValue = Number(ethers.utils.formatEther(weiBN));
-    return etherValue;
-  }
+  const weiString = wei.toString();
+  const weiBN = ethers.BigNumber.from(weiString);
+  const etherValue = Number(ethers.utils.formatEther(weiBN));
+  return etherValue;
 }
 
 
 export function filterData(arr: any[]){
-  return arr.map(async (res) => {
+  return arr.map((res) => {
     const { offerer, recipient, offer, consideration, data } = res;
-    const { transactionHash } = data;
 
+    console.log("=====offerData Results======");
+    console.log(offer);
+    console.log("=====offerData Results======");
+    
+    const { transactionHash } = data;
+// need to fix this to only pick offer array with type 2
     const offerData = offer.map((offerRes: any []) => {
       if (offerRes[0] === 2) {
         return {
@@ -64,37 +55,26 @@ export function filterData(arr: any[]){
       }
     });
 
-    // console.log("=====offerData Results======");
-    // console.log(offerData);
-    // console.log("=====offerData Results======");
     
 
     const considerationData = consideration.map((considerationRes: any[]) => {
       if (considerationRes[0] === 0) {
-        // if the consideration is in empty, then remove the consideration
-        
+        // if the consideration is in empty, then remove the
         return {
           price: hexToDecimal(considerationRes[3]?._hex),
+          sender: considerationRes[4],
         };
       }
     });
 
-
-    // await createSaleRecordOnDB({
-    //   transactionHash,
-    //   offerer,
-    //   recipient,
-    //   ...offerData[0],
-    //   ...considerationData[0],
-    // });
-
     return {
       offerer,
       recipient,
-      contractAddress: offerData[0]?.contractAddress,
+      contractAddress: offerData[0].contractAddress,
       tokenId: offerData[0]?.tokenId,
       quantity: offerData[0]?.quantity,
       price: weiToEther((considerationData[0]?.price)),
+      sender: considerationData[0]?.sender,
       transactionHash,
     };
   });
